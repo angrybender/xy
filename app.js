6c3cd9b486b3bd8d46179acb1a7bcb91e483940c
angrybender <dev.temp.null.mail@gmail.com>
2015-04-08 22:54:16 +0300

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $(function() {
         $('#container div:last').css({
             width: widths[Math.round(Math.random()*3)],
             height: height[Math.round(Math.random()*3)],
-            background: 'url(http://lorempixel.com/200/200?' + Math.random() + ') cover'
+            background: 'url(http://lorempixel.com/200/200?' + Math.random() + ') center cover'
         });
     }
 
