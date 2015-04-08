1e38f1ada06a920dcfb68a0b0d472d3b13756f17
angrybender <dev.temp.null.mail@gmail.com>
2015-04-08 22:50:58 +0300

    fix

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 $(function() {
 
     $('#container').width(1000).height(1000);
-    var widths = [50, 100, 200];
+    var widths = [100, 150, 300];
     var height = [50, 100, 200];
 
     for (var i = 0; i < 20; i++) {
         $('#container').append('<div class="item"></div>');
         $('#container div:last').css({
             width: widths[Math.round(Math.random()*3)],
-            height: widths[Math.round(Math.random()*3)],
+            height: height[Math.round(Math.random()*3)],
             background: 'url(http://lorempixel.com/200/200?' + Math.random() + ') center'
         });
     }
